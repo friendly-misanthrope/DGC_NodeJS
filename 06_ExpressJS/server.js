d1884652bda6b150849470f16674889cd569f720
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-14 02:03:21 -0700

    chapter 6 - building a web server using Express.js

--- 06_ExpressJS/server.js
+++ 06_ExpressJS/server.js
@@ -1,123 +1,32 @@
-// NodeJS Common Core modules
-const http = require('http');
+const express = require('express');
 const path = require('path');
-const fs  = require('fs');
-const fsPromises = require('fs').promises;
-
-// Custom/third-party modules
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-
-class Emitter extends EventEmitter {};
-
-// Initialize emitter object
-const myEmitter = new Emitter();
-
-// Event emitter 
-myEmitter.on('log', (msg, fileName) => {
-  logEvents(msg, fileName);
-});
-
-// Set port
 const PORT = process.env.PORT || 3500;
 
-const serveFile = async (filePath, contentType, res) => {
-  try {
-    const rawData = await fsPromises.readFile(
-      filePath,
-      !contentType.includes('image') ?
-        'utf8'
-        : ''
-    );
-    const data = contentType === 'application/json' ? JSON.parse(rawData)
-      : rawData;
-    res.writeHead(
-      filePath.includes('404.html') ? 404
-        : 200,
-      {'Content-Type': contentType}
-      );
-    res.end(
-      contentType === 'application/json'
-        ? JSON.stringify(data)
-        : data
-    );
-  } catch(e) {
-    console.error(e);
-    myEmitter.emit('log', `${e.name}: ${e.message}`, 'errLog.txt');
-    res.statusCode = 500;
-    res.end();
-  }
-}
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
-  const extension = path.extname(req.url);
-
-  let contentType;
-  switch(extension) {
-    case '.css':
-      contentType = 'text/css';
-      break;
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.json':
-      contentType = 'application/json';
-      break;
-    case '.jpg':
-      contentType = 'image/jpeg';
-      break;
-    case '.png':
-      contentType = 'image/png';
-      break;
-    case '.txt':
-      contentType = 'text/plain';
-      break;
-    default:
-      contentType = 'text/html';
-  }
-
-  let filePath =
-    contentType === 'text/html' && req.url === '/'
-      ? path.join(__dirname, 'views', 'index.html')
-      : contentType === 'text/html' && req.url.slice(-1) === '/'
-        ? path.join(__dirname, 'views', req.url, 'index.html')
-        : contentType === 'text/html'
-          ? path.join(__dirname, 'views', req.url)
-          : path.join(__dirname, req.url);
-
-  // .html ext not required in browser if below is included:
-  if (!extension && req.url.slice(-1) !== '/'){
-    filePath += '.html';
-  }
-
-  const fileExists = fs.existsSync(filePath);
-  if (fileExists) {
-    // Serve the file
-    serveFile(filePath, contentType, res);
-  } else {
-    // error/redirect/etc
-    switch(path.parse(filePath).base){
-      case 'old-page.html':
-        res.writeHead(301, {'Location': './new-page.html'});
-        res.end();
-        break;
-
-      case 'www-page.html':
-        res.writeHead(301, {'Location': '/'});
-        res.end();
-        break;
-      
-      default:
-        // Serve a 404 response
-        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-
-    }
-    console.log(path.parse(filePath));
-  }
+// HTTP routes & handlers
+app.get('^/$|/index(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-server.listen(PORT, () => {
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+//! Sending 304 instead of 301 when redirects - why?
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page');
+});
+
+app.get('/hello(.html)?', (req, res, next) => {
+  console.log('attempted to load hello.html');
+})
+
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+// Listen for HTTP requests on specified port
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
