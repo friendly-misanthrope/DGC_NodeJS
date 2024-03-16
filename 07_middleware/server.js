5f1198fdaf1b766a6f940ad821954607e7f7a7e4
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-16 05:35:44 -0700

    Implemented CORS policy

--- 07_middleware/server.js
+++ 07_middleware/server.js
@@ -1,18 +1,34 @@
 const express = require('express');
 const path = require('path');
-const logEvents = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
+const cors = require('cors');
 const PORT = process.env.PORT || 3500;
 const app = express();
 
-// Custom middleware (logger)
-// Waterfall cascades to each route below middleware
-app.use((req, res, next) => {
-  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
-  console.log(`${req.method} ${req.path}`);
-  next();
-});
 
-// Built-in middleware
+/* CUSTOM MIDDLEWARE */
+// Event logger
+app.use(logger);
+
+
+/* THIRD-PARTY MIDDLEWARE */
+// CORS whitelist
+const allowedDomains = ['http://localhost:3500', 'https://localhost:3500', 'https://127.0.0.1:3500', 'http://127.0.0.1:3500'];
+
+// CORS policy
+const corsOptions = {
+  origin: (reqOrigin, cb) => {
+    !reqOrigin || allowedDomains.indexOf(reqOrigin) !== -1 ?
+      cb(null, true)
+      : cb(new Error("CORS policy has blocked this request"));
+  },
+
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+
+/* BUILT-IN EXPRESS MIDDLEWARE */
 // allows form data to be handled via URL
 app.use(express.urlencoded({ extended: false }));
 
@@ -22,7 +38,8 @@ app.use(express.json());
 // allow serving of static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// HTTP routes & handlers
+
+/* HTTP ROUTES AND HANDLERS */
 app.get('^/$|/index(.html)?', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
@@ -39,13 +56,14 @@ app.get('/old-page(.html)?', (req, res) => {
 app.get('/hello(.html)?', (req, res, next) => {
   res.send("Hello, this is the hello page!");
   console.log('attempted to load hello.html');
-})
+});
 
 app.get('/*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-// Listen for HTTP requests on specified port
+
+/* LISTEN FOR INCOMING REQUESTS ON SPECIFIED PORT */
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
