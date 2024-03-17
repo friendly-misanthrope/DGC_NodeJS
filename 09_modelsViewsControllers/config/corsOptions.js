ef818ebd525abe0ca2a97a7c2196ae171e68ac8c
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-17 00:36:06 -0700

    removed CORS allowance for google

--- 09_modelsViewsControllers/config/corsOptions.js
+++ 09_modelsViewsControllers/config/corsOptions.js
@@ -1,9 +1,9 @@
 // CORS whitelist
-const allowedDomains = ['https://localhost:3500', 'http://localhost:3500', 'https://www.google.com'];
+const allowedDomains = ['https://localhost:3500', 'http://localhost:3500'];
 
 const corsOptions = {
   origin: (reqOrigin, cb) => {
-    allowedDomains.indexOf(reqOrigin) !== -1 ?
+    !reqOrigin || allowedDomains.indexOf(reqOrigin) !== -1 ?
       cb(null, true)
       : cb(new Error('CORS policy has blocked this request'));
   },
