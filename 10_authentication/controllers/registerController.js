b4c9c2b79b2fbfcb8d1ca39cc421b31da1fd5ad7
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-17 23:48:09 -0700

    removed unused imports

--- 10_authentication/controllers/registerController.js
+++ 10_authentication/controllers/registerController.js
@@ -1,10 +1,6 @@
 const Employees = require('./employeesController');
 const Users = require('../models/user.model');
 
-const fsPromises = require('fs').promises;
-const path = require('path');
-const bcrypt = require('bcrypt');
-
 
 const registerUser = async (req, res) => {
   // ToDo: 
