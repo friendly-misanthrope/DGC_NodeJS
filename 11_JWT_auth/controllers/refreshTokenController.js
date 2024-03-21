de157e34ad3a7c990f7bb09c1a3818557144b451
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-21 03:40:18 -0700

    fixed bugs and cleaned up comments

--- 11_JWT_auth/controllers/refreshTokenController.js
+++ 11_JWT_auth/controllers/refreshTokenController.js
@@ -3,36 +3,40 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const userRefreshToken = async (req, res) => {
-  // get user and password from login form
+  // get cookies from request
   const cookies = req.cookies;
+  // If no cookies or no JWT, rtn 401 unauthorized
   if (!cookies?.jwt) {
     return res.status(401);
   }
-  console.log(cookies.jwt);
+  
+  // Create refresh token
   const refreshToken = cookies.jwt;
+
   try {
-    // Check if provided username exists in DB
-    const foundUser = await Users.findOne({ username: user });
-    // If user isn't found, return 401 unauthorized
+    // Find user based on username from req.cookies.jwt
+    const foundUser = await Users.findOne({ username: refreshToken.username });
+    // If username from refreshToken isn't found in DB, rtn 401 unauthorized
     if (!foundUser) {
-      return res.status(403).json({message: "Invalid credentials"});
+      return res.status(401);
     }
-    // Verify refresh token
+    // Verify refresh token with private key
     jwt.verify(
       refreshToken,
       process.env.REFRESH_TOKEN_SECRET,
       (err, decoded) => {
         // if access token not valid
         if (err || foundUser.username !== decoded.username) {
+          // rtn 403 forbidden
           return res.sendStatus(403);
         }
-        // otherwise, create new access token
+        // otherwise, sign a new access token
         const accessToken = jwt.sign(
           { "username": decoded.username },
           process.env.ACCESS_TOKEN_SECRET,
           { expiresIn: '5m' }
         );
-        // send token in response
+        // send new access token in response
         res.json({ accessToken });
       }
     )
