3fd7a2f38af51aeddaa4a719eb6eb08133970c7b
friendly-misanthrope <nickthedevguy@gmail.com>
2024-03-23 02:24:42 -0700

    fixed bug where refreshToken payload was not properly decoded. Access and refresh tokens are now being issued properly.

--- 11_JWT_auth/controllers/refreshTokenController.js
+++ 11_JWT_auth/controllers/refreshTokenController.js
@@ -3,17 +3,47 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const userRefreshToken = async (req, res) => {
+  // get cookies from request
   const cookies = req.cookies;
-  if (!cookies.jwt) {
-    return res.status(403)
+  // If no cookies or no JWT, rtn 401 unauthorized
+  if (!cookies?.jwt) {
+    return res.status(401);
   }
+  // Create refresh token
   const refreshToken = cookies.jwt;
-  console.log(refreshToken)
-  try {
-    // get user based on username
-    const foundUser = await Users.findOne({username: "Nick"})
-  } catch(e) {
 
+  try {
+    // Decode refreshToken payload, save username
+    const username = JSON.parse(atob(refreshToken.split('.')[1])).username;
+    // Find user based on username in refresh token
+    const foundUser = await Users.findOne({ username: username });
+    // If username from refreshToken isn't found in DB, rtn 401 unauthorized
+    if (!foundUser) {
+      return res.status(401);
+    }
+    // Verify refresh token with private key
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      (err, decoded) => {
+        // if access token not valid
+        if (err || foundUser.username !== decoded.username) {
+          // rtn 403 forbidden
+          return res.sendStatus(403);
+        }
+        // otherwise, sign a new access token
+        const accessToken = jwt.sign(
+          { "username": decoded.username },
+          process.env.ACCESS_TOKEN_SECRET,
+          { expiresIn: '5m' }
+        );
+        // send new access token in response
+        res.json({accessToken: accessToken });
+      }
+    )
+    // error handling
+  } catch(e) {
+    res.status(401).json({error: e});
   }
 }
 
