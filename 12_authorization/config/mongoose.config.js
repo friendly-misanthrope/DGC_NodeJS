f9529d08a5da54c9a81437b2067aa3cb23554f31
Nick Barker <nickthedevguy@gmail.com>
2024-03-24 01:26:18 -0700

    Deleted duplicate

--- 12_authorization/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/DG_ExpressBackend')
-  .then(() => console.log('Database connection established'))
-  .catch((err) => {
-    console.log('Connection to database failed', err);
-  })
