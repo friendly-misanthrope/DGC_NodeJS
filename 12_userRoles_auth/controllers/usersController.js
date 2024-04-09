41071611408313f303ff7ae1e71388ae8194bf14
friendly-misanthrope <nickthedevguy@gmail.com>
2024-04-09 02:22:45 -0700

    Created controller function for getting all users with their respective employee

--- 12_userRoles_auth/controllers/usersController.js
+++ 12_userRoles_auth/controllers/usersController.js
@@ -5,17 +5,17 @@ const Employees = require('../models/employees.model');
 const getAllUsersWithEmployee = async (req, res) => {
   try {
     const users = await Users.find();
-    const allUsers = users.map(async (user) => {
+    const allUsersWithEmployee = await Promise.all(users.map(async (user) => {
       // Get employee for current user
-      // const employeeId = user.employee;
-      const employeeObj = await Employees.findOne({ _id: user.employee })
-      console.log(employeeObj)
+      const employeeObj = await Employees.findOne({ _id: user.employee });
+      user.employee = employeeObj;
+      return user;
+    }));
 
-    })
+    res.status(200).json(allUsersWithEmployee)
   } catch (e) {
     res.status(500).json({ message: "Unable to get all users with their employee" })
   }
-
 }
 
 
