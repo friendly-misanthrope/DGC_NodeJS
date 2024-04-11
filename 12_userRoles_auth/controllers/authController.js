1357e1f574928042bcb911071047e5acae327223
friendly-misanthrope <nickthedevguy@gmail.com>
2024-04-10 22:14:00 -0700

    changed JWT payloads to email

--- 12_userRoles_auth/controllers/authController.js
+++ 12_userRoles_auth/controllers/authController.js
@@ -5,14 +5,14 @@ require('dotenv').config();
 
 const userLogin = async (req, res) => {
   // get user and password from login form
-  const { user, pwd } = req.body;
+  const { email, pwd } = req.body;
   // Check that both fields are present
-  if (!user || !pwd) {
-    return res.status(400).json({ message: 'Username and password are required' });
+  if (!email || !pwd) {
+    return res.status(400).json({ message: 'Email and password are required' });
   }
   try {
-    // Check if provided username exists in DB
-    const foundUser = await Users.findOne({ username: user });
+    // Check if provided email exists in DB
+    const foundUser = await Users.findOne({ email: email });
     // If user isn't found, return 401 unauthorized
     if (!foundUser) {
       return res.status(401).json({message: "Invalid credentials"});
@@ -23,21 +23,21 @@ const userLogin = async (req, res) => {
       // create access token
       const accessToken = jwt.sign(
         // Payload - no sensitive data
-        { "username": foundUser.username },
+        { "email": foundUser.email },
         process.env.ACCESS_TOKEN_SECRET,
         { expiresIn: '5m' }
       );
       // create refresh token
       const refreshToken = jwt.sign(
         // Payload - no sensitive data
-        { "username": foundUser.username },
+        { "email": foundUser.email },
         process.env.REFRESH_TOKEN_SECRET,
         { expiresIn: '1d' }
       );
       // Update user in DB to include refreshToken
       const currentUser = { ...foundUser, refreshToken };
       await Users.findOneAndUpdate(
-        { username: foundUser.username },
+        { email: foundUser.email },
         { currentUser },
         { runValidators: true }
       );
--- 12_userRoles_auth/controllers/registerController.js
+++ 12_userRoles_auth/controllers/registerController.js
@@ -6,23 +6,23 @@ const registerUser = async (req, res) => {
   try {
     // Check if user attempting to be created already exists
     const potentialUser = await Users.findOne({
-      username: req.body.username
+      email: req.body.email
     })
-    // if username exists in DB,
+    // if email exists in DB,
     if (potentialUser) {
       // Return 409 conflict && an error message
-      return res.status(409).json({ message: `${potentialUser.username} already exists.` });
+      return res.status(409).json({ message: `${potentialUser.email} already exists.` });
     } else {
       // otherwise, create new user from req.body
       const {
-        username,
+        email,
         password,
         confirmPassword,
         firstName,
         lastName } = req.body;
 
       const newUser = await Users.create({
-        username,
+        email,
         password,
         confirmPassword,
         // and assign it a new Employee instance
@@ -34,10 +34,10 @@ const registerUser = async (req, res) => {
       // If user and employee were created successfully,
       // return 201 created && send new user object in response
       // ToDo: remove user (at least password field) from success response
-      res.status(201).json({ message: `${newUser.username} added to system`, newUser });
+      res.status(201).json({ message: `${newUser.email} added to system`, newUser });
     }
   } catch (err) {
-    res.status(400).json({ message: `Could not add ${username}`, err });
+    res.status(400).json({ message: `Could not add ${email}`, err });
   }
 }
 
