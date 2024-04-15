65cf9fb26c030eb5185f96577aea7a2b900c3506
friendly-misanthrope <nickthedevguy@gmail.com>
2024-04-14 23:14:38 -0700

    created transporter, options for pw reset email

--- 12_userRoles_auth/controllers/forgotPasswordController.js
+++ 12_userRoles_auth/controllers/forgotPasswordController.js
@@ -5,22 +5,35 @@ const mailer = require('nodemailer');
 require('dotenv').config();
 
 const forgotPassword = async (req, res) => {
-
   try {
+    // Get user based on email
     const { email } = req.body;
     const user = await Users.findOne({ email: email });
-
+    // If no user in DB, return status 400 & error message
     if (!user) {
       return res.status(400).json({ message: `No user with email ${email} found` });
     }
-
+    // Otherwise, generate a JWT reset token
     const resetToken = jwt.sign(
       { "email": user.email },
       process.env.RESET_PASSWORD_SECRET,
       { expiresIn: '15min' }
     );
 
-    // let transporter = mailer.createTransport()
+    let transporter = mailer.createTransport({
+      service: 'gmail',
+      port: 465,
+      auth: {
+        user: process.env.GOOGLE_APP_EMAIL,
+        pass: process.env.GOOGLE_APP_PW
+      }
+    });
+
+    const emailOptions = {
+      from: process.env.GOOGLE_APP_EMAIL,
+      to: email,
+      subject: 'ERPeezy Password Reset'
+    }
 
     await Users.updateOne({ resetLink: resetToken })
 
