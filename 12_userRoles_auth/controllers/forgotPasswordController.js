3e9cfd1a5bca62b28944e69c686944d525ab589c
friendly-misanthrope <nickthedevguy@gmail.com>
2024-04-14 22:42:48 -0700

    added try/catch

--- 12_userRoles_auth/controllers/forgotPasswordController.js
+++ 12_userRoles_auth/controllers/forgotPasswordController.js
@@ -5,7 +5,31 @@ const mailer = require('nodemailer');
 require('dotenv').config();
 
 const forgotPassword = async (req, res) => {
-  
+
+  try {
+    const { email } = req.body;
+    const user = await Users.findOne({ email: email });
+
+    if (!user) {
+      return res.status(400).json({ message: `No user with email ${email} found` });
+    }
+
+    const resetToken = jwt.sign(
+      { "email": user.email },
+      process.env.RESET_PASSWORD_SECRET,
+      { expiresIn: '15min' }
+    );
+
+    // let transporter = mailer.createTransport()
+
+    await Users.updateOne({ resetLink: resetToken })
+
+    const decodedToken = JSON.parse(atob(resetToken.split('.')[1]))
+    res.status(200).json(decodedToken)
+  } catch (e) {
+    res.status(400).json({ message: `Unable to send password reset email to ${email}` })
+  }
+
 }
 
 module.exports = {
